d2322b1e1e5f898a9fe681b6c6338e3a4bc3479d
CMarLar <c.marina.larrosa@gmail.com>
2022-11-11 20:25:38 +0100

    Actualización ejercicios dia 4

--- dia3/proyecto api/src/routers/professionals.routers.js
+++ dia3/proyecto api/src/routers/professionals.routers.js
@@ -1,8 +1,17 @@
 const {Router} = require('express');
 const router = Router();
 const profCtrl = require("../controller/proArray.controller");
+//front:
+const frontCtrl = require("../index")
+
+router.get('/prueba', frontCtrl.getProfessionals)
 
 
+
+
+
+
+//ejercicios anteriores
 router.get('/profesionales', profCtrl.getProfessional);
 
 //router.get('/usuario/:id', profCtrl.getUserParams);
